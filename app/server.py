b228be5eea0cdaea7d7ffb35539fb265acd3fd58
simpleitis <66766238+simpleitis@users.noreply.github.com>
2020-06-16 08:01:02 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-8Hpd5C1waYNH_txASespof8mXKW9Yqq'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['meme','image']
 path = Path(__file__).parent
 
 app = Starlette()
